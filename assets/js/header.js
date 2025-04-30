691e41000f64c38fd1e404ac461eee18c34c9750
pwang8888 <103546322+pwang8888@users.noreply.github.com>
2025-04-30 16:59:42 -0700

    Update header.js

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,8 +1,19 @@
 document.querySelector('header').innerHTML = `
-  <a href="index.html" class="logo" title="Go to homepage">👩🏻‍🎨</a>
+  <a href="index.html" class="logo" title="Go to homepage">PW</a>
   <nav>
-    <a href="portfolio.html">Portfolio</a>
+    <a href="index.html">Home</a>
+    <a href="portfolio.html">Work</a>
     <a href="about.html">About</a>
     <a href="contact.html">Contact</a>
   </nav>
 `;
+
+// Add scroll effect to header
+window.addEventListener('scroll', () => {
+  const header = document.querySelector('header');
+  if (window.scrollY > 50) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});

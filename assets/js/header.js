4f129d90b594599b78d000dd498d62e8372ec4be
pwang8888 <103546322+pwang8888@users.noreply.github.com>
2025-04-30 16:08:59 -0700

    Create header.js

---
+++ assets/js/header.js
@@ -0,0 +1,8 @@
+document.querySelector('header').innerHTML = `
+  <a href="index.html" class="logo" title="Go to homepage">👩🏻‍🎨</a>
+  <nav>
+    <a href="portfolio.html">Portfolio</a>
+    <a href="about.html">About</a>
+    <a href="contact.html">Contact</a>
+  </nav>
+`;

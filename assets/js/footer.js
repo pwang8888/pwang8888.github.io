f466a9e9a63a9b075ca95aa28805233524eeb3ac
pwang8888 <103546322+pwang8888@users.noreply.github.com>
2025-04-30 16:09:28 -0700

    Create footer.js

---
+++ assets/js/footer.js
@@ -0,0 +1,7 @@
+document.querySelector('footer').innerHTML = `
+  <div>© 2025 Penny Wang</div>
+  <div class="social">
+    <a href="https://twitter.com" target="_blank"><i class="fab fa-twitter"></i></a>
+    <a href="https://linkedin.com" target="_blank"><i class="fab fa-linkedin"></i></a>
+  </div>
+`;

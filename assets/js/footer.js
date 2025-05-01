ccd28c0ba63f528655a2ff20b9e45a94c2223e1b
pwang8888 <103546322+pwang8888@users.noreply.github.com>
2025-04-30 17:19:31 -0700

    Add files via upload

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,17 +1,7 @@
 document.querySelector('footer').innerHTML = `
-  <div>© ${new Date().getFullYear()} Penny Wang</div>
+  <div>© 2025 Penny Wang</div>
   <div class="social">
-    <a href="https://github.com" target="_blank" rel="noopener noreferrer" title="GitHub">
-      <i class="fab fa-github"></i>
-    </a>
-    <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" title="LinkedIn">
-      <i class="fab fa-linkedin-in"></i>
-    </a>
-    <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" title="Twitter">
-      <i class="fab fa-twitter"></i>
-    </a>
-    <a href="mailto:contact@pennywang.com" title="Email">
-      <i class="far fa-envelope"></i>
-    </a>
+    <a href="https://twitter.com" target="_blank"><i class="fab fa-twitter"></i></a>
+    <a href="https://linkedin.com" target="_blank"><i class="fab fa-linkedin"></i></a>
   </div>
 `;
--- assets/js/header.js
+++ assets/js/header.js
@@ -1,19 +1,8 @@
 document.querySelector('header').innerHTML = `
-  <a href="index.html" class="logo" title="Go to homepage">PW</a>
+  <a href="index.html" class="logo" title="Go to homepage">👩🏻‍🎨</a>
   <nav>
-    <a href="index.html">Home</a>
-    <a href="portfolio.html">Work</a>
+    <a href="portfolio.html">Portfolio</a>
     <a href="about.html">About</a>
     <a href="contact.html">Contact</a>
   </nav>
 `;
-
-// Add scroll effect to header
-window.addEventListener('scroll', () => {
-  const header = document.querySelector('header');
-  if (window.scrollY > 50) {
-    header.classList.add('scrolled');
-  } else {
-    header.classList.remove('scrolled');
-  }
-});
